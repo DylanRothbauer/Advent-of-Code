19c3f1792dc88aa7019383cb8d2c7fe6c06bb599
DylanRothbauer <djrothbauer@gmail.com>
2024-12-04 22:20:15 -0600

    We did it! Day 3 complete!

--- 2024/Day 3/main.cpp
+++ 2024/Day 3/main.cpp
@@ -6,17 +6,38 @@
 using namespace std;
 
 bool isValidNumber(const string& str) {
+
     if (str.empty()) {
         return false;
     }
 
-    for (int i = 0; i < str.size(); i++) {
+    for (int i = 0; i < str.length(); i++) {
         char ch = str[i];
         if (!isdigit(ch)) {
             return false;
         }
     }
     return true;
+}
+
+int processMul(const string& content) {
+    // Check if another "mul(" exists inside the content; if so, abandon processing
+    if (content.find("mul(") != string::npos) {
+        return 0;  // Invalid due to nested "mul("
+    }
+
+    int commaPos = content.find(',');
+    if (commaPos != string::npos && content.find(',', commaPos + 1) == string::npos) {
+        string num1_str = content.substr(0, commaPos);
+        string num2_str = content.substr(commaPos + 1);
+
+        if (isValidNumber(num1_str) && isValidNumber(num2_str)) {
+            int num1 = stoi(num1_str);
+            int num2 = stoi(num2_str);
+            return num1 * num2;
+        }
+    }
+    return 0;  // Return 0 if invalid
 }
 
 int main() {
@@ -31,23 +52,19 @@ int main() {
     bool mulEnabled = true;
 
     while (getline(inFile, inputLine)) {
-        //inputLine = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-    //inputLine = "why()$mul(5,5)don't()mul(5,5)do()mul(2,2)^?!what()";
         int pos = 0;
 
         while (pos < inputLine.length()) {
-            // Check for do() and don't()
             if (inputLine.substr(pos, 7) == "don't()") {
                 mulEnabled = false;
                 pos += 7;
-                //cout << "POS: " << pos << endl;
-            } else if (inputLine.substr(pos, 4) == "do()") {
+            }
+            else if (inputLine.substr(pos, 4) == "do()") {
                 mulEnabled = true;
                 pos += 4;
-                //cout << "POS: " << pos << endl;
-            } else if (inputLine.substr(pos, 4) == "mul(" && mulEnabled) {
+            }
+            else if (inputLine.substr(pos, 4) == "mul(" && mulEnabled) {
                 int start = pos + 4;
-                //cout << "POS: " << pos << endl;
                 int openBrackets = 1;
                 int end = start;
 
@@ -59,25 +76,13 @@ int main() {
                 }
 
                 if (openBrackets == 0) {  // Valid "mul()"
-                    string mulExpr = inputLine.substr(start, end - start - 1);  // Extract inside mul()
-                    size_t commaPos = mulExpr.find(',');
-
-                    if (commaPos != string::npos) {
-                        string num1_str = mulExpr.substr(0, commaPos);
-                        string num2_str = mulExpr.substr(commaPos + 1);
-
-                        if (isValidNumber(num1_str) && isValidNumber(num2_str)) {
-                            int num1 = stoi(num1_str);
-                            int num2 = stoi(num2_str);
-                            cout << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
-                            sum += (num1 * num2);
-                        }
-                    }
+                    string mulExpr = inputLine.substr(start, end - start - 1);
+                    sum += processMul(mulExpr);  // Process the extracted content
                 }
-                pos = end; // Move past the current "mul()"
+                pos++;  // Move past the current "mul()"
             }
             else {
-                pos++;  // Move  to the next character
+                pos++;
             }
         }
     }
